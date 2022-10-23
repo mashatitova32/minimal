8bd652344d0a3ed02ed13576e4ca4e521f582b50
Masha Titova <masha.titova32@gmail.com>
2022-10-23 18:52:03 +0300

    slider + style

--- js/index.js
+++ js/index.js
@@ -8,19 +8,30 @@ const settingSlider = {
     sliderToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    fade: true,
+    cssEase: 'linear',
+    centerMode: true,
+    centerPadding: '60px',
     mobiLeftFirst: true,
     responsive: [
         {
-            breakpoint: 400,
+            breakpoint: 480,
             setting: {
                 sliderToShow: 2,
                 sliderToScroll: 2
             }
+        }, {
+            breakpoint: 760,
+            setting: {
+                sliderToShow: 2,
+                sliderToScroll: 1
+            }
         },
-        {
-            breakpoint: 1024,
-            setting: 'unslick'
-        },
+
+        // {
+        //     breakpoint: 1024,
+        //     setting: 'unslick'
+        // },
     ]
 }
 
